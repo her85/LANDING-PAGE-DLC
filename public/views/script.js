36b2b251e55583464c86e473e0fc2a65e495ab9f
HerHub <hab_85@live.com.ar>
2025-01-31 09:04:26 -0300

    js

---
+++ public/views/script.js
@@ -0,0 +1,5 @@
+document.getElementById('formulario-descarga').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para procesar la descarga
+    alert('¡Gracias por descargar Valorant Mario Bros!');
+});
